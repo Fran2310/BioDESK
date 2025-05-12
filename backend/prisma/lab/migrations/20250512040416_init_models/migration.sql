46a8d135dd3ebbe06ce6b53d133463fe1fe1641a
JoshuaCarrera <cubyt3@gmail.com>
2025-05-12 00:45:56 -0400

    Optimizar ids en los schemas

--- backend/prisma/lab/migrations/20250512040416_init_models/migration.sql
+++ backend/prisma/lab/migrations/20250512040416_init_models/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "LabUser" (
     "id" SERIAL NOT NULL,
-    "systemUuid" TEXT NOT NULL,
+    "systemUserUuid" UUID NOT NULL,
     "roleId" INTEGER,
 
     CONSTRAINT "LabUser_pkey" PRIMARY KEY ("id")
@@ -18,7 +18,7 @@ CREATE TABLE "Role" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "LabUser_systemUuid_key" ON "LabUser"("systemUuid");
+CREATE UNIQUE INDEX "LabUser_systemUserUuid_key" ON "LabUser"("systemUserUuid");
 
 -- AddForeignKey
 ALTER TABLE "LabUser" ADD CONSTRAINT "LabUser_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- backend/prisma/system/migrations/20250512043835_init_models/migration.sql
+++ backend/prisma/system/migrations/20250512043835_init_models/migration.sql
@@ -1,30 +1,7 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Role` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-  - Made the column `rif` on table `Lab` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_labId_fkey";
-
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_roleId_fkey";
-
--- AlterTable
-ALTER TABLE "Lab" ALTER COLUMN "rif" SET NOT NULL;
-
--- DropTable
-DROP TABLE "Role";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "SystemUser" (
     "id" SERIAL NOT NULL,
-    "uuid" TEXT NOT NULL,
+    "uuid" UUID NOT NULL,
     "ci" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
@@ -38,11 +15,32 @@ CREATE TABLE "SystemUser" (
     CONSTRAINT "SystemUser_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Lab" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "dbName" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "rif" TEXT NOT NULL,
+    "dir" TEXT,
+    "phoneNums" TEXT,
+    "logoPath" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Lab_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "SystemUser_uuid_key" ON "SystemUser"("uuid");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "SystemUser_ci_key" ON "SystemUser"("ci");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SystemUser_email_key" ON "SystemUser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Lab_dbName_key" ON "Lab"("dbName");
 
 -- AddForeignKey
 ALTER TABLE "SystemUser" ADD CONSTRAINT "SystemUser_labId_fkey" FOREIGN KEY ("labId") REFERENCES "Lab"("id") ON DELETE SET NULL ON UPDATE CASCADE;
