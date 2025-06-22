fc8dbac8ee027a29ab9ce9f416dab41b738398d5
Jsssus <jesusrhivan@gmail.com>
2025-06-21 21:48:07 -0400

    actualizando

---
+++ frontend/src/pages/laboratory/composables/useReferenceManager.ts
@@ -0,0 +1,37 @@
+    //modal for managing references and variations
+import { useReferenceModal } from './useReferenceModal';
+
+const {
+    showModal,
+    showVariationModal,
+    referenceData,
+    selectedReference,
+    isEditingReference,
+    selectedReferenceIndex,
+    selectedVariation,
+    selectedVariationIndex,
+    isEditingVariation
+} = useReferenceModal();
+
+
+export function saveReference() {
+    if (isEditingReference.value) {
+        if (selectedReferenceIndex.value !== null) {
+        referenceData.value[selectedReferenceIndex.value] = { ...selectedReference.value };
+        }
+    } else {
+        referenceData.value.push({ ...selectedReference.value });
+    }
+    showModal.value = false;
+}
+
+function editReference(index:number) {
+    selectedReferenceIndex.value = index;
+    selectedReference.value = { ...referenceData.value[index] };
+    isEditingReference.value = true;
+    showModal.value = true;
+}
+
+function removeReference(index:number) {
+    referenceData.value.splice(index, 1);
+}
