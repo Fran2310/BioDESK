5af2f14160456a37e5133ecf4dde448c494ba4bb
Ferid-Richtofen <pj.marcano2004@gmail.com>
2025-06-30 14:04:14 -0400

    Rol dinámico - banner

--- frontend/biodesk-app/src/views/home/HomeView.vue
+++ frontend/biodesk-app/src/views/home/HomeView.vue
@@ -5,7 +5,7 @@
     v-for="(content, idx) in banner"
     :key="idx"
     :labName="content.labName"
-    :role="content.role"
+    :role="content.role ? content.role.charAt(0).toUpperCase() + content.role.slice(1) : ''"
   />
   
   <div class="card-container">
@@ -37,21 +37,44 @@
 <script setup lang="ts">
   import { ref, onMounted } from 'vue'
   import { useLabStore } from '@/stores/labStore'
+  import { userApi } from '@/services/api'
   import CardSimple from './CardSimple.vue'
   import CardComplex from './CardComplex.vue'
   import BannerHome from './BannerHome.vue'
+import { GetWithPermissionsQuerys } from '@/services/interfaces/global'
 
   // Contenido del banner
   const labStore = useLabStore();
   const banner = ref([
     {
       labName: '',
-      role: 'Admin'
+      role: ''
     }
   ])
 
   onMounted(async () => {
-    banner.value[0].labName = labStore.currentLab.name    // escribe el nombre en el banner
+    banner.value[0].labName = labStore.currentLab.name    // asigna el valor de lab para el banner
+
+    // Conseguir el rol del usuario
+    try {
+      const { data } = await userApi.getMe();
+      const query:GetWithPermissionsQuerys = {
+        'search-fields': ['email'],
+        'search-term': data.email,
+        offset: 0,
+        limit: 1,
+        includePermissions: false
+      };
+
+      const response = await userApi.getUsersMix(query)
+      console.log('Response: ', response.data)
+      const user = response.data.data[0];
+ 
+      console.log('User:', user)
+      banner.value[0].role = user?.labUser.role?.role || 'Rol desconocido';   // asigna el valor del rol para el banner
+    } catch (error) {
+      banner.value[0].role = 'Rol desconocido';
+    }
   })
 
   // Contenido para las tarjetas simples
